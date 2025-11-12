51e2df0308dcebdb6a0d04a779ca7adc3bdb5973
mashoodgondal <mashoodgondal6146@gmail.com>
2025-11-12 16:08:45 +0500

    use beforeEach method to go fartly

--- test/checkout.spec.ts
+++ test/checkout.spec.ts
@@ -4,34 +4,49 @@ import { LoginPage } from "../pages/loginPage";
 import { InventoryPage } from "../pages/inventory";
 import { CartPage } from "../pages/cartPage";
 test.describe('test checkout page', () => {
-  test('check visibulity', async ({ page }) => {
-    const loginPage = new LoginPage(page)
-    const checkoutPage = new CheckoutPage(page);
-    const inventoryPage = new InventoryPage(page)
-    const cartPage= new CartPage(page)
-    await loginPage.goto()
-    await loginPage.login("standard_user", "secret_sauce");
-  
-    await inventoryPage.addProductToCart("Sauce Labs Backpack");
-    await inventoryPage.openCart()
-    await cartPage.goToCheckout()
+  let inventoryPage:InventoryPage
+  let checkoutPage:CheckoutPage
+  let loginPage: LoginPage
+  let cartPage: CartPage
 
-    await checkoutPage.CheckoutPageVisible()
+test.beforeEach('run before every test',async({page})=>{
+        loginPage = new LoginPage(page)
+        checkoutPage= new CheckoutPage(page)
+        inventoryPage = new InventoryPage(page)
+        cartPage = new CartPage(page)
+        await loginPage.goto()
+        await loginPage.login("standard_user", "secret_sauce")
+        await inventoryPage.addProductToCart("Sauce Labs Backpack")
+        await inventoryPage.openCart()
+        await cartPage.goToCheckout()
+})
+  test('check visibulity', async ({ page }) => {
+    // const loginPage = new LoginPage(page)
+    // const checkoutPage = new CheckoutPage(page);
+    // const inventoryPage = new InventoryPage(page)
+    // const cartPage= new CartPage(page)
+    // await loginPage.goto()
+    // await loginPage.login("standard_user", "secret_sauce");
+    // await inventoryPage.addProductToCart("Sauce Labs Backpack");
+    // await inventoryPage.openCart()
+    // await cartPage.goToCheckout()
+
+    // await checkoutPage.CheckoutPageVisible()
     await checkoutPage.FillForm("ani","adu","se32")
     await checkoutPage.ClickButton()
     await checkoutPage.CheckNextPagevisibulity()
   });
   test('check error visibulity',async({page})=>{
-    const loginPage = new LoginPage(page)
-    const checkoutPage = new CheckoutPage(page);
-    const inventoryPage = new InventoryPage(page)
-    const cartPage = new CartPage(page)
-    await loginPage.goto()
-    await loginPage.login("standard_user", "secret_sauce");
-    await inventoryPage.addProductToCart("Sauce Labs Backpack");
-    await inventoryPage.openCart()
-    await cartPage.goToCheckout()
-    await checkoutPage.CheckoutPageVisible()
+    // const loginPage = new LoginPage(page)
+    // const checkoutPage = new CheckoutPage(page);
+    // const inventoryPage = new InventoryPage(page)
+    // const cartPage = new CartPage(page)
+    // await loginPage.goto()
+    // await loginPage.login("standard_user", "secret_sauce");
+    // await inventoryPage.addProductToCart("Sauce Labs Backpack");
+    // await inventoryPage.openCart()
+    // await cartPage.goToCheckout()
+    // await checkoutPage.CheckoutPageVisible()
     await checkoutPage.FillForm("ani","","se32")
     await checkoutPage.ClickButton()
     await checkoutPage.VerifyError()
