3178f5383f2469c5b9498b921925375d83724f92
mashoodgondal <mashoodgondal6146@gmail.com>
2025-12-03 18:48:48 +0500

    change test

--- test/login.spec.ts
+++ test/login.spec.ts
@@ -37,4 +37,9 @@ test.describe("SauceDemo Login Tests", () => {
     //   await loginPage.goto();
     //   await loginPage.login("invalid_user", "wrong_pass");
     //   await loginPage.verifyLoginError();
-    // });
+    // });  // test("should show error with invalid credentials", async ({ page }) => {
+      //   const loginPage = new LoginPage(page);
+      //   await loginPage.goto();
+      //   await loginPage.login("invalid_user", "wrong_pass");
+      //   await loginPage.verifyLoginError();
+      // });
