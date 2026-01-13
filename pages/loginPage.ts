33bccd85413f2dc71c997354d9e2925aca39650b
mashoodgondal <mashoodgondal6146@gmail.com>
2026-01-13 23:35:04 +0500

    rendom testing

--- pages/loginPage.ts
+++ pages/loginPage.ts
@@ -38,3 +38,8 @@ export class LoginPage {
   //   await this.page.fill("#password", password);
   //   await this.page.click("#login-button");
   // }
+  // async login(username: string, password: string) {
+    //   await this.page.fill("#user-name", username);
+    //   await this.page.fill("#password", password);
+    //   await this.page.click("#login-button");
+    // }
