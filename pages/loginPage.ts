552360093f0715db5c8953b7de646fddc8ed03df
mashoodgondal <mashoodgondal6146@gmail.com>
2026-01-21 23:58:52 +0500

    num

--- pages/loginPage.ts
+++ pages/loginPage.ts
@@ -37,3 +37,10 @@ export class LoginPage {
 // async verifyLoginError() {
 //   await expect(this.page.getByText("Epic sadface")).toBeVisible();
 // }
+
+// await this.page.click("#login-button");
+// }
+
+// async verifyLoginError() {
+//   await expect(this.page.getByText("Epic sadface")).toBeVisible();
+// }
