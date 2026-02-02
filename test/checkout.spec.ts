e8fd28c54b4a8d539ffcb550eb18ef4fa850ab04
mashoodgondal <mashoodgondal6146@gmail.com>
2026-02-02 23:03:46 +0500

    test for verifying

--- test/checkout.spec.ts
+++ test/checkout.spec.ts
@@ -36,3 +36,11 @@ test.beforeEach('run before every test',async({page})=>{
 
 
 
+
+// test('check error visibulity',async({page})=>{
+    
+//   await checkoutPage.FillForm("ani","","se32")
+//   await checkoutPage.ClickButton()
+//   await checkoutPage.VerifyError()
+
+// })
