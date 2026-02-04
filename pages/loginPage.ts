cc8ed765200e1bb2376537bc113d7b3cf79961d2
mashoodgondal <mashoodgondal6146@gmail.com>
2026-02-04 23:19:10 +0500

    A test for check valiaation

--- pages/loginPage.ts
+++ pages/loginPage.ts
@@ -29,18 +29,3 @@ export class LoginPage {
 
 
 
-
-//   async goto() {
-  //     await this.page.goto("https://www.saucedemo.com/");
-  //   }
-  
-  //   async login(username: string, password: string) {
-  //     await this.page.fill("#user-name", username);
-  //     await this.page.fill("#password", password);
-  //     await this.page.click("#login-button");
-  //   }
-    
-  //   async verifyLoginError() {
-  //     await expect(this.page.getByText("Epic sadface")).toBeVisible();
-  //   }
-  // }
