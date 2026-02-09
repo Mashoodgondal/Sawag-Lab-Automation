73a89b8bf86dbf75f5af87fcf7200636feeb8782
mashoodgondal <mashoodgondal6146@gmail.com>
2026-02-09 22:01:26 +0500

    test case for remove product

--- pages/cartPage.ts
+++ pages/cartPage.ts
@@ -27,15 +27,3 @@ export class CartPage {
 
 
 
-
-
-
-
-// async removeProduct(productName: string) {
-  //   const removeButton = this.page.locator(`xpath=//div[text()="${productName}"]/../../..//button`);
-  //   await removeButton.click();
-  // }
-  // async goToCheckout(){
-  //   const button = this.page.locator('#checkout')
-  //   await button.click()
-  // }
